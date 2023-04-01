623d1d25b6edcf297914d2a91de716e06e608354
2A5F <U2A5F@outlook.com>
2023-04-01 14:21:25 +0800

    运行器配置

--- grass/src/config.ts
+++ grass/src/config.ts
@@ -1,3 +1,4 @@
+import { Runtime } from '@makegrass/grass-runner'
 import { Schema } from 'koishi'
 
 export interface Config {
@@ -13,11 +14,18 @@ export interface Config {
     life: boolean
     runner: {
       self: boolean
-      js: boolean
-      node: boolean
     }
   }
+  runner: {
+    shortcuts: { name: string; runtime: string }[]
+    cpus?: number
+    memory?: number | string
+    memorySwap?: number | string
+    timeout?: number
+  }
 }
+
+const RunnerRuntime: Schema<Runtime> = Schema.union([Schema.const('node' as const), Schema.const('node-eval' as const)])
 
 export const Config: Schema<Config> = Schema.intersect([
   Schema.object({
@@ -26,24 +34,39 @@ export const Config: Schema<Config> = Schema.intersect([
         read: Schema.boolean().default(false).description('是否启用自动已读所有消息'),
         friend: Schema.boolean().default(false).description('是否启用自动通过好友申请'),
         guild: Schema.boolean().default(false).description('是否启用自动通过入群邀请'),
-      }).description('验证器'),
+      }),
     }),
-  }),
+  }).description('验证器'),
   Schema.object({
     enable: Schema.object({
       runner: Schema.object({
         self: Schema.boolean().default(false).description('是否启用代码运行器'),
-        js: Schema.boolean().default(false).description('是否启用 js'),
-        node: Schema.boolean().default(false).description('是否启用 node'),
-      }).description('代码运行器'),
-    }).description('工具功能'),
-  }),
+      }),
+    }),
+    runner: Schema.object({
+      shortcuts: Schema.array(
+        Schema.object({
+          name: Schema.string(),
+          runtime: RunnerRuntime,
+        })
+      )
+        .default([
+          { name: 'js', runtime: 'node-eval' },
+          { name: 'node', runtime: 'node' },
+        ])
+        .description('别名设置'),
+      cpus: Schema.number().default(1).description('cpu 限制'),
+      memory: Schema.string().default('128m').description('内存限制'),
+      memorySwap: Schema.string().default('512m').description('总内存(内存+虚拟内存)限制'),
+      timeout: Schema.number().default(60).description('超时时间，单位秒'),
+    }),
+  }).description('代码运行器'),
   Schema.object({
     enable: Schema.object({
       choose: Schema.boolean().default(false).description('是否启用怎么选'),
       random: Schema.boolean().default(false).description('是否启用随机数'),
       setu: Schema.boolean().default(false).description('是否启用涩图'),
       life: Schema.boolean().default(false).description('是否启用生活'),
-    }).description('娱乐功能'),
-  }),
+    }),
+  }).description('娱乐功能'),
 ])
--- grass/src/runner.tsx
+++ grass/src/runner.tsx
@@ -1,42 +1,55 @@
 import { Context, Session } from 'koishi'
 import { Config } from './config'
 import { run, hasRuntime } from '@makegrass/grass-runner'
+import { logger } from './logger'
 
 export function runner(ctx: Context, config: Config) {
-  ctx
+  let cmd = ctx
     .command('执行 <code:text>')
     .option('runtime', '-r <rt:string>', { value: 'node-eval' })
-    .action(({ session, options }, code) => runCode(session, options.runtime, code))
-    .shortcut('js', { options: { runtime: 'node-eval' }, fuzzy: true })
-    .shortcut('node', { options: { runtime: 'node' }, fuzzy: true })
+    .action(({ session, options }, code) => runCode(session, options.runtime, code, config))
+  for (const { name, runtime } of config.runner.shortcuts) {
+    cmd = cmd.shortcut(name, { options: { runtime }, fuzzy: true })
+  }
 }
 
-async function runCode(session: Session<never, never>, rt: string, code: string) {
+async function runCode(session: Session<never, never>, rt: string, code: string, config: Config) {
   if (!code?.trim()) {
     session.send(session.text('.no-code'))
     return
   }
+  code = code.replaceAll('&lt;', '<').replaceAll('&gt;', '>').replaceAll('&amp;', '&')
   if (!hasRuntime(rt)) {
     session.send(session.text('.unknow-rt'))
     return
   }
   try {
+    logger.info('[执行环境] %s', rt)
+    logger.info('[执行输入] %s', code)
     const r = await run(
       {
         runtime: rt,
         code,
+        cpus: config.runner.cpus,
+        memory: config.runner.memory,
+        memorySwap: config.runner.memorySwap,
+        timeout: config.runner.timeout,
       },
       msg => {
-        session.send(<code>{msg}</code>)
+        logger.info('[执行输出] %s', `${msg}`)
+        session.send(msg.trim() == '' ? session.text('.noret') : msg)
       },
       err => {
-        session.send(<code>{err}</code>)
+        logger.error('[执行输出] %s', `${err}`)
+        session.send(err.trim() == '' ? session.text('.noret') : err)
       }
     )
     if (!r) {
+      logger.info('[执行输出] 无返回或超时')
       session.send(session.text('.noret'))
     }
   } catch (e) {
-    session.send(<code>{e}</code>)
+    logger.error('%o', e)
+    session.send(e)
   }
 }
